772238dbcc39d65e5ba6a7116fe783683dca7d32
Rendi Nurhidayat <rendiuser50@gmail.com>
2025-10-27 15:55:37 +0700

    final deployment

--- constants.ts
+++
@@ -1,4 +0,0 @@
-
-// Data dari file ini telah dipindahkan ke services/api.ts
-// untuk membuat service API lebih mandiri dan siap untuk migrasi backend.
-// File ini dapat dihapus dengan aman dari proyek.
--- pages/staff/PracticalClassManagerPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AdminPracticalClassManagerPage from '../admin/PracticalClassManagerPage';
-
-// Staff can reuse the same component as Admin for class management
-const StaffPracticalClassManagerPage = () => {
-  return <AdminPracticalClassManagerPage />;
-};
-
-export default StaffPracticalClassManagerPage;
